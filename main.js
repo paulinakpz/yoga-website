75c90eef065e6fff4f7ec394a00d95ae2b81cddd
paulinakpz <paulinakwapisz6@gmail.com>
2021-07-19 23:52:35 +0200

    styling scroll up button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener('scroll', showScroll)
 
 function showScroll() {
     const scrollup = document.getElementById('scroll-up');
-    if (window.scrollY > 250) {
+    if (window.scrollY > 200) {
         scrollup.classList.remove('d-none');
     }
     else {
