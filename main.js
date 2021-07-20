674f7547883d0f87758d0f953a7f7eac6e47f2ae
paulinakpz <paulinakwapisz6@gmail.com>
2021-07-20 14:30:42 +0200

    using lodash library to delay showScroll function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-document.addEventListener('scroll', showScroll)
+document.addEventListener('scroll', _.debounce(showScroll, 500))
 
 function showScroll() {
     const scrollup = document.getElementById('scroll-up');
+
     if (window.scrollY > 200) {
         scrollup.classList.remove('d-none');
+        
     }
     else {
         scrollup.classList.add('d-none');
